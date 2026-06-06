95af12ea93728f0fcdfafbeb259512ee40bfe88e
sofianeO1 <ouatikisofiane1000@gmail.com>
2026-06-06 21:36:57 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("🎧 EDM Ravers Bot è online!")
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
